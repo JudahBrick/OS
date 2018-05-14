dc53661ab31a1709a78428aff98ee63eece56ba7
Yehuda Brick <yehudazvi.brick@gmail.com>
2018-05-14 14:04:43 -0400

    another error attempt fix

--- hello_proc/hello_proc.c
+++ hello_proc/hello_proc.c
@@ -33,7 +33,7 @@ static struct file_operations hello_proc_ops = {
    .write = procfile_write,
 };
 
-char* rem = kmalloc(80);
+char* rem = kmalloc(80 * sizeOf(char));
 
 /* This function is called to create the special proc file entry on 
  * module load.  This file is created as /proc/helloworld. */
